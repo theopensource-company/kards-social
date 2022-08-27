ac9b5c96f7e220cc94ffece1cc1433988d857d51
Micha de Vries <micha@devrie.sh>
2022-08-27 16:40:06 +0200

    Fixed linting

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <h1>Hi there!</h1>
-  )
-}
+  return <h1>Hi there!</h1>;
+};
 
 export default Home;
