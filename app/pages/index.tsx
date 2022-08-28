ab7c405442a66567de246207f7fce19db3224d41
Morgan Hofmann <morgan@theopensource.company>
2022-08-28 10:04:19 +0200

    Start on landing page (#1)
    * Start on landing page
    
    Designed in accordance with Figma design, signup link is not functional yet.
    
    * Fixed linting
    
    * Alt text added.
    
    * Disabled image optimization because the check apparently doesn't allow this to go unnoticed.
    
    * Little step in making it more mobile friendly
    
    * Fix monorepo stuff :D
    
    * Correct license
    
    * Fixed small css bugs
    
    Co-authored-by: Micha de Vries <micha@devrie.sh>

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
-const Home: NextPage = () => {
-  return <h1>Hi there!</h1>;
+import Button from "../components/Button";
+
+import LogoVector from "../assets/image/Logo.svg";
+import styles from "../styles/Landing.module.scss";
+
+const Landing: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className="image-frame">
+          <Image src={LogoVector} alt="The Kards logo" />
+        </div>
+
+        <h1>Opening up soon</h1>
+        <span>You can reserve your spot soon</span>
+        {/* <Link href="/signup">
+          <a>Reserve your spot now</a>
+        </Link> */}
+
+        <p>
+          Kards is an open social media platform, free from abusive data
+          harvesting and damaging algorithms.
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Landing;
