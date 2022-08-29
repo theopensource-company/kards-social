dfc374fc97243820c5d6dd14888ca3f82369ab98
Micha de Vries <micha@devrie.sh>
2022-08-29 17:02:59 +0200

    Added layout

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,28 +6,31 @@ import Button from "../components/Button";
 
 import LogoVector from "../assets/image/Logo.svg";
 import styles from "../styles/Landing.module.scss";
+import Layout from "../components/Layout";
 
 const Landing: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className="image-frame">
-          <Image src={LogoVector} alt="The Kards logo" />
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className="image-frame">
+            <Image src={LogoVector} alt="The Kards logo" />
+          </div>
+
+          <h1>Opening up soon</h1>
+          <span>
+            <Link href="/join-waitlist">
+              <a>Reserve your spot now</a>
+            </Link>
+          </span>
+
+          <p>
+            Kards is an open social media platform, free from abusive data
+            harvesting and damaging algorithms.
+          </p>
         </div>
-
-        <h1>Opening up soon</h1>
-        <span>
-          <Link href="/join-waitlist">
-            <a>Reserve your spot now</a>
-          </Link>
-        </span>
-
-        <p>
-          Kards is an open social media platform, free from abusive data
-          harvesting and damaging algorithms.
-        </p>
       </div>
-    </div>
+    </Layout>
   );
 };
 
