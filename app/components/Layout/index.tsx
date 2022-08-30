3710fca00640755824487d403436fafd1b94f004
Micha de Vries <micha@devrie.sh>
2022-08-30 14:39:46 +0200

    Added layout (#5)
    * Added layout
    
    * Changed naming to proper brand
    
    * Missed the naming at the top, changed
    
    * Motto!
    
    * Added layout
    
    * Changed naming to proper brand
    
    * Missed the naming at the top, changed
    
    * Motto!
    
    * Changed the rest of the titles
    
    Co-authored-by: Morgan Hofmann <morgan@morganhofmann.nl>

---
+++ app/components/Layout/index.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import Head from "next/head";
+import { ReactNode } from "react";
+
+type Props = {
+  title?: string;
+  description?: string;
+  robots?: string;
+  children: ReactNode;
+};
+
+export default function Layout({
+  title,
+  description,
+  robots,
+  children,
+}: Props) {
+  return (
+    <>
+      <Head>
+        <title>{title ? `${title} - Kards` : "Kards: Socially Social"}</title>
+
+        <meta name="charset" content="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1"
+        />
+        <meta name="robots" content={robots ?? "index, follow"} />
+
+        <meta
+          name="title"
+          content={title ? `${title} - Kards` : "Kards: Socially Social"}
+        />
+
+        <meta
+          name="og:title"
+          content={title ? `${title} - Kards` : "Kards: Socially Social"}
+        />
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="https://kards.social/" />
+        <meta
+          name="og:image"
+          itemProp="image primaryImageOfPage"
+          content="https://kards.social/Logo.png"
+        />
+        <meta
+          name="og:site_name"
+          content={title ? `${title} - Kards` : "Kards: Socially Social"}
+        />
+
+        <meta
+          name="twitter:title"
+          content={title ? `${title} - Kards` : "Kards: Socially Social"}
+        />
+        <meta name="twitter:card" content="summary" />
+
+        <meta
+          name="description"
+          content={
+            description ??
+            "Kards is an open social media platform, free from abusive data harvesting and damaging algorithms. Join the waitlist now!"
+          }
+        />
+        <meta
+          name="og:description"
+          content={
+            description ??
+            "Kards is an open social media platform, free from abusive data harvesting and damaging algorithms. Join the waitlist now!"
+          }
+        />
+        <meta
+          name="twitter:description"
+          content={
+            description ??
+            "Kards is an open social media platform, free from abusive data harvesting and damaging algorithms. Join the waitlist now!"
+          }
+        />
+      </Head>
+      <div id="app-container">{children}</div>
+    </>
+  );
+}
--- app/pages/joined-waitlist.tsx
+++ app/pages/joined-waitlist.tsx
@@ -6,17 +6,20 @@ import Image from "next/image";
 
 import styles from "../styles/JoinWaitlist.module.scss";
 import Logo from "../assets/image/Logo.svg";
+import Layout from "../components/Layout";
 
 export default function JoinWaitlist() {
   const router = useRouter();
 
   return (
-    <Container>
-      <div className={styles.form}>
-        <Image src={Logo} alt="Kards logo" />
-        <p className={styles.success}>You have been added to the waitlist!</p>
-        <Button onClick={() => router.push("/")} text="Go back" />
-      </div>
-    </Container>
+    <Layout>
+      <Container>
+        <div className={styles.form}>
+          <Image src={Logo} alt="Kards logo" />
+          <p className={styles.success}>You have been added to the waitlist!</p>
+          <Button onClick={() => router.push("/")} text="Go back" />
+        </div>
+      </Container>
+    </Layout>
   );
 }
