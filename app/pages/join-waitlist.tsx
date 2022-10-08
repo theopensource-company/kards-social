a79931bcb300839067e2901844cfba05f0ca49b1
Micha de Vries <micha@devrie.sh>
2022-10-08 10:08:38 +0200

    Revamped the waitlist form

--- app/pages/join-waitlist.tsx
+++ app/pages/join-waitlist.tsx
@@ -1,70 +1,67 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Container from "../components/Container";
-import InputField from "../components/InputField";
 import Button from "../components/Button";
 import ArrowBack from "../components/icon/ArrowBack";
-import Spinner from "../components/icon/Spinner";
 import { toast } from "react-toastify";
 
 import styles from "../styles/JoinWaitlist.module.scss";
 import axios from "axios";
-import Link from "next/link";
 import Layout from "../components/Layout";
 import Logo from "../components/Logo";
+import { Form } from "../components/Form";
+import { FormInputField } from "../components/Form/InputField";
+import { TForm } from "../constants/Types";
 
 export default function JoinWaitlist() {
   const router = useRouter();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [nameLabel, setNameLabel] = useState("");
-  const [emailLabel, setEmailLabel] = useState("");
   const [working, setWorking] = useState(false);
 
-  const submit = async () => {
-    setNameLabel("");
-    setEmailLabel("");
-    let valid = true;
-    if (!/^[A-ZÀ-ÖØ-öø-ÿ]+ [A-ZÀ-ÖØ-öø-ÿ][A-ZÀ-ÖØ-öø-ÿ ]*$/i.test(name)) {
-      valid = false;
-      setNameLabel("Please enter your full name");
-    }
+  const submitForm: TForm["onSubmit"] = async ({ values, faulty }) => {
+    if (faulty.name) toast.error("Please enter your full name");
+    if (faulty.email) toast.error("Please enter a valid email");
+    if (Object.keys(faulty).length > 0) return;
 
-    if (
-      !/^[A-ZÀ-ÖØ-öø-ÿ0-9._%+-]+@[A-ZÀ-ÖØ-öø-ÿ0-9.-]+\.[A-Z]{2,}$/i.test(email)
-    ) {
-      valid = false;
-      setEmailLabel("Please enter a valid email");
-    }
-
-    if (valid) {
-      setWorking(true);
-      try {
-        const result: any = await axios.post(
-          `${location.origin}/api/waitlist/join`,
-          {
-            name,
-            email,
-            origin: location.origin,
-          }
-        );
-
-        if (!result.data)
-          return toast.error(
-            "Something went wrong, please try again later or contact hi@kards.social"
-          );
-        if (result.data?.success) {
-          toast.success("Check you inbox and spam for a verification email!");
-        } else {
-          toast.error(`${result.data?.message} (${result.data?.error})`);
+    setWorking(true);
+    try {
+      const result: any = await axios.post(
+        `${location.origin}/api/waitlist/join`,
+        {
+          name: values.name,
+          email: values.email,
+          origin: location.origin,
         }
-      } catch (e) {
-        toast.error("An error occured while performing the request.");
-      }
+      );
 
-      setWorking(false);
+      if (!result.data)
+        return toast.error(
+          "Something went wrong, please try again later or contact hi@kards.social"
+        );
+      if (result.data?.success) {
+        toast.success("Check you inbox and spam for a verification email!");
+      } else {
+        toast.error(`${result.data?.message} (${result.data?.error})`);
+      }
+    } catch (e) {
+      toast.error("An error occured while performing the request.");
     }
+
+    setWorking(false);
   };
+
+  const inputName = new FormInputField({
+    name: "name",
+    placeholder: "Full Name",
+    isValid: (value) =>
+      /^[A-ZÀ-ÖØ-öø-ÿ]+ [A-ZÀ-ÖØ-öø-ÿ][A-ZÀ-ÖØ-öø-ÿ ]*$/i.test(value),
+  });
+
+  const inputEmail = new FormInputField({
+    name: "email",
+    placeholder: "Email",
+    isValid: (value) =>
+      /^[A-ZÀ-ÖØ-öø-ÿ0-9._%+-]+@[A-ZÀ-ÖØ-öø-ÿ0-9.-]+\.[A-Z]{2,}$/i.test(value),
+  });
 
   return (
     <Layout>
@@ -77,25 +74,18 @@ export default function JoinWaitlist() {
         />
       </div>
       <Container className={styles.container}>
-        <div className={styles.form}>
+        <Form
+          className={styles.form}
+          inputs={[inputName, inputEmail]}
+          onSubmit={submitForm}
+        >
           <Logo />
           <div className={styles.inputs}>
-            <InputField
-              value={name}
-              label={nameLabel}
-              setValue={setName}
-              placeholder="Full Name"
-            />
-            <InputField
-              value={email}
-              label={emailLabel}
-              setValue={setEmail}
-              placeholder="Email"
-              type="Email"
-            />
+            <inputName.render />
+            <inputEmail.render />
           </div>
-          <Button onClick={submit} text="Join waitlist" loading={working} />
-        </div>
+          <Button text="Join waitlist" loading={working} />
+        </Form>
       </Container>
     </Layout>
   );
