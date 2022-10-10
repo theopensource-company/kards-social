7340c2ec8fd13b26f41c64a638a057e3c6e954e8
Micha de Vries <micha@devrie.sh>
2022-10-10 17:24:29 +0200

    Shutdown devserver when a process is unreachable

--- cli/devserver.js
+++ cli/devserver.js
@@ -10,10 +10,21 @@ const target = {
 
 const appProxy = express();
 const proxyserver = httpProxy.createProxyServer({ target: target.app, ws: true });
+const safeProxy = (req, res, opt) => {
+    try {
+        let res = proxyserver.web(req, res, opt);
+        return res;
+    } catch(e) {
+        console.log("One of the processes seems to be unavailable, closing the devserver.");
+        return exit();
+    }
+}
 
-appProxy.all("/api/waitlist/*", (req, res) => proxyserver.web(req, res, {target: target.waitlistApi}));
-appProxy.all("/api/user/*", (req, res) => proxyserver.web(req, res, {target: target.userApi}));
-appProxy.all("/*", (req, res) => proxyserver.web(req, res, {target: target.app}));
+appProxy.all("/api/waitlist/*", (req, res) => safeProxy(req, res, {target: target.waitlistApi}));
+appProxy.all("/api/user/*", (req, res) => safeProxy(req, res, {target: target.userApi}));
+appProxy.all("/*", (req, res) => safeProxy(req, res, {target: target.app}));
+
+// All workers but the first one have to wait 1 second to make sure that the first worker started. Otherwise the workers will start to complain about previously used ports.
 
 const runners = concurrently(
     [
@@ -23,33 +34,34 @@ const runners = concurrently(
         },
         {
             name: "kards-worker-waitlist",
-            command: "cd workers/waitlist && wrangler dev --env=dev"
+            command: "cd workers/waitlist && wrangler dev --env=dev --experimental-local"
         },
         {
             name: "kards-worker-user",
-            command: "cd workers/user && wrangler dev --env=dev"
+            command: "sleep 1; cd workers/user && wrangler dev --env=dev --experimental-local"
         },
     ],
 );
 
+let appProxyServer;
 setTimeout(() => {
-    const appProxyServer = appProxy.listen(12000, () => console.log('[PROXY-APP] APP: http://localhost:12000'));
+    appProxyServer = appProxy.listen(12000, () => console.log('[PROXY-APP] APP: http://localhost:12000'));
     appProxyServer.on('upgrade', (req, socket, head) => proxyserver.ws(req, socket, head));
+}, 5000);
 
-    async function exit() {
-        console.log("Caught interrupt signal");
+async function exit() {
+    console.log("Caught interrupt signal");
 
-        for(var i = 0; i < runners.commands.length; i++) {
-            console.log(`Closing runner "${runners.commands[i].name}".`);
-            await runners.commands[i].kill();
-        }
-
-        console.log('Closing proxy for app');
-        appProxyServer.close();
-
-        process.exit(0);
+    for(var i = 0; i < runners.commands.length; i++) {
+        console.log(`Closing runner "${runners.commands[i].name}".`);
+        await runners.commands[i].kill();
     }
 
-    process.on('uncaughtException', exit);
-    process.on('SIGINT', exit);
-}, 5000);
+    console.log('Closing proxy for app');
+    appProxyServer.close();
+
+    setTimeout(() => process.exit(0), 1000);
+}
+
+process.on('uncaughtException', exit);
+process.on('SIGINT', exit);
