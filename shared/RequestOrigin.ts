1b827334ad8896f47541450b44a14d06b6120978
Micha de Vries <micha@devrie.sh>
2022-11-11 21:01:42 +0100

    Remove shared code for workers

--- shared/RequestOrigin.ts
+++
@@ -1,9 +0,0 @@
-export function RequestOrigin(request: {
-    url: string;
-}, env: {
-    KARDS_ORIGIN?: string;
-}) {
-    return env.KARDS_ORIGIN ?? (new URL(request.url)).origin;
-}
-
-export default RequestOrigin;
