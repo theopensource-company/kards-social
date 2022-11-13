1e485c243e3a1f5379e361233ce7fa36ec95eabf
Micha de Vries <micha@devrie.sh>
2022-11-13 15:10:08 +0100

    Create account privacy & security pages

---
+++ app/pages/account/security.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+
+import {
+    useAuthState,
+} from '../../hooks/KardsUser';
+import AccountLayout from '../../components/Layout/Account';
+
+export default function Account() {
+    const router = useRouter();
+    const auth = useAuthState();
+
+    useEffect(() => {
+        if (!auth.authenticated) {
+            toast.info('Please signin first!');
+            router.push('/auth/signin');
+        }
+    }, [auth, router]);
+
+    return (
+        <AccountLayout activeKey='security'>
+            {auth.details && (
+                <div>
+                    Security
+                </div>
+            )}
+        </AccountLayout>
+    );
+}
