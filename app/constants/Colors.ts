3346060df68dd3a1d4c213fb4d2f2aea3f797a9e
Micha de Vries <micha@devrie.sh>
2022-11-15 18:10:18 +0100

    Update colors

--- app/constants/Colors.ts
+++ app/constants/Colors.ts
@@ -1,7 +1,26 @@
-export type BasicColorType = 'Light' | 'Dark';
-export type ColorType = 'Tint' | 'Light' | 'Dark';
 export const Color = {
     Tint: '#F98C10',
+
     Light: '#F7F3E3',
+    LightTintLight: '#ffffff',
+    LightTintLightHover: '#F7F3E3',
+    LightTintDark: '#F2EBCF',
+    LightTintDarkHover: '#EDE4BF',
+
     Dark: '#0C1618',
+    DarkTintLight: '#152529',
+    DarkTintLightHover: '#1C3136',
+    DarkTintDark: '#070C0E',
+    DarkTintDarkHover: '#000000',
+
+    Blue: '#009FF5',
+    Red: '#F24236',
+    Green: '#1EAE5A',
 };
+
+export type BasicColorType = 'Light' | 'Dark';
+export type ColorType = 'Tint' | 'Light' | 'Dark';
+export type OtherColor = 'Blue' | 'Red' | 'Green';
+export type ColorTint = keyof Omit<typeof Color, ColorType | OtherColor>;
+export type ColorTintType = 'Light' | 'LightHover' | 'Dark' | 'DarkHover';
+export type BasicColorTintType = 'Light' | 'Dark';
