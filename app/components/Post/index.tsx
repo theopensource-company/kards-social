621e4f26e03251c705d58903f2fb602eb6d4d2c4
Morgan Hofmann <morgan@morganhofmann.nl>
2022-12-29 19:57:02 +0100

    Initial commit

--- app/components/Post/index.tsx
+++ app/components/Post/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 
 import styles from '../../styles/components/Post.module.scss';
@@ -26,46 +26,17 @@ export default function Post({ image, caption }: Props) {
             "The post didn't have a caption or image associated."
         );
 
-    const [flipped, setFlipped] = useState<boolean>(false);
-
-    const postClicked = (e: React.MouseEvent<HTMLElement>) => {
-        const target = e.target as HTMLElement;
-
-        // If the clicked target is preventing the flip, don't flip.
-        if (
-            target.getAttribute('data-preventflip') ||
-            (target.parentElement &&
-                target.parentElement.getAttribute('data-preventflip'))
-        )
-            return;
-
-        setFlipped(!flipped);
-    };
-
-    const classes = [styles.default, flipped ? styles.flipped : null]
+    const classes = [styles.default]
         .filter((a) => !!a)
         .join(' ');
 
-    console.log(image);
-
     return (
-        <div className={classes} onClick={postClicked}>
-            <div className={styles.card}>
-                <div className={styles.front}>
-                    <div className={styles.image}>
-                        <Image src={image} alt="" fill={true} />
-                    </div>
-                </div>
-                <div className={styles.back}>
-                    <div className={styles.image}>
-                        <Image src={image} alt="" fill={true} />
-                    </div>
-                    <span className={styles.caption} data-preventflip={true}>
-                        <Linkify componentDecorator={LinkComponent}>
-                            {caption}
-                        </Linkify>
-                    </span>
-                </div>
+        <div className={classes}>
+            <div className={styles.images}></div>
+            <div className={styles.post}>
+                <div className={styles.author}></div>
+                <div className={styles.caption}></div>
+                <div className={styles.comments}></div>
             </div>
         </div>
     );
